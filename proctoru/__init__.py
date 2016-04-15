acff01c55f00edc9e69d8f784637ee057a3b098b
Dadaso Zanzane <dadaso.zanzane@cuelogic.co.in>
2016-04-15 19:15:08 +0530

    add_to_builtin method revert for funmooc

--- proctoru/__init__.py
+++ proctoru/__init__.py
@@ -1,7 +1,3 @@
 # do not include any code here, since this module is loaded by the LMS/CMS
 # settings.
 
-from .proctoru import ProctorUXBlock
-from django.template.loader import add_to_builtins
-
-add_to_builtins('proctoru.templatetags.proctoru_validator')
