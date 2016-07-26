b93fbc430c1983548aa0dfd33693b4a79a605d19
Richard Moch <Richard.moch@gmail.com>
2016-07-26 12:11:53 +0000

    News Django 1.8 migration

--- proctoru/migrations/0001_initial.py
+++ proctoru/migrations/0001_initial.py
@@ -1,134 +1,52 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+import django.core.validators
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'ProctoruUser'
-        db.create_table('proctoru_proctoruuser', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('student', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
-            ('phone_number', self.gf('django.db.models.fields.CharField')(max_length=100, blank=True)),
-            ('time_zone', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('address', self.gf('django.db.models.fields.TextField')()),
-            ('city', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('country', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('date_created', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-        ))
-        db.send_create_signal('proctoru', ['ProctoruUser'])
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
 
-        # Adding model 'ProctorUAuthToken'
-        db.create_table('proctoru_proctoruauthtoken', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('token', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('date_created', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-            ('enabled', self.gf('django.db.models.fields.BooleanField')(default=True)),
-        ))
-        db.send_create_signal('proctoru', ['ProctorUAuthToken'])
-
-        # Adding model 'ProctorUExam'
-        db.create_table('proctoru_proctoruexam', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
-            ('start_date', self.gf('django.db.models.fields.DateTimeField')()),
-            ('actual_start_time', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
-            ('is_completed', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('is_started', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('is_canceled', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('course_id', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('end_time', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
-            ('reservation_id', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('reservation_no', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('url', self.gf('django.db.models.fields.TextField')()),
-        ))
-        db.send_create_signal('proctoru', ['ProctorUExam'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'ProctoruUser'
-        db.delete_table('proctoru_proctoruuser')
-
-        # Deleting model 'ProctorUAuthToken'
-        db.delete_table('proctoru_proctoruauthtoken')
-
-        # Deleting model 'ProctorUExam'
-        db.delete_table('proctoru_proctoruexam')
-
-
-    models = {
-        'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        'auth.permission': {
-            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        'auth.user': {
-            'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
-        },
-        'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        'proctoru.proctoruauthtoken': {
-            'Meta': {'object_name': 'ProctorUAuthToken'},
-            'date_created': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'enabled': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'token': ('django.db.models.fields.CharField', [], {'max_length': '200'})
-        },
-        'proctoru.proctoruexam': {
-            'Meta': {'object_name': 'ProctorUExam'},
-            'actual_start_time': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
-            'course_id': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
-            'end_time': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_canceled': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_completed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_started': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'reservation_id': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'reservation_no': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
-            'start_date': ('django.db.models.fields.DateTimeField', [], {}),
-            'url': ('django.db.models.fields.TextField', [], {}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
-        },
-        'proctoru.proctoruuser': {
-            'Meta': {'object_name': 'ProctoruUser'},
-            'address': ('django.db.models.fields.TextField', [], {}),
-            'city': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'country': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'date_created': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'phone_number': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
-            'student': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
-            'time_zone': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        }
-    }
-
-    complete_apps = ['proctoru']
+    operations = [
+        migrations.CreateModel(
+            name='ProctorUExam',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('start_date', models.DateTimeField(verbose_name='heure de d&eacute;part')),
+                ('actual_start_time', models.DateTimeField(null=True, verbose_name='Heure de d&eacute;but r&eacute;elle', blank=True)),
+                ('is_completed', models.BooleanField(default=False)),
+                ('is_started', models.BooleanField(default=False)),
+                ('is_canceled', models.BooleanField(default=False)),
+                ('block_id', models.CharField(max_length=200)),
+                ('end_time', models.DateTimeField(null=True, verbose_name='heure de fin', blank=True)),
+                ('reservation_id', models.CharField(max_length=50)),
+                ('reservation_no', models.CharField(max_length=200)),
+                ('url', models.TextField()),
+                ('user', models.ForeignKey(related_name='proctoru_exams', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='ProctoruUser',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('phone_number', models.CharField(blank=True, max_length=100, validators=[django.core.validators.RegexValidator(regex=b'^\\+?1?\\d{9,15} |  \\(\\d{3}\\)[-]\\d{3}[-\\.]??\\d{4}$ | \\d{3}[-]\\d{3}[-\\.]??\\d{4}$', message='Num&eacute;ro de t&eacute;l&eacute;phone doit &ecirc;tre saisi dans le format: &apos;999999999&apos;. Jusqu&apos;&agrave; 15 chiffres autoris&eacute;s.')])),
+                ('time_zone', models.CharField(max_length=100)),
+                ('time_zone_display_name', models.CharField(max_length=100)),
+                ('address', models.TextField()),
+                ('city', models.CharField(max_length=50)),
+                ('country', models.CharField(max_length=50)),
+                ('state', models.CharField(max_length=50)),
+                ('date_created', models.DateTimeField(auto_now=True)),
+                ('student', models.ForeignKey(related_name='proctoru_user', to=settings.AUTH_USER_MODEL, unique=True)),
+            ],
+        ),
+        migrations.AlterUniqueTogether(
+            name='proctoruexam',
+            unique_together=set([('user', 'block_id')]),
+        ),
+    ]
